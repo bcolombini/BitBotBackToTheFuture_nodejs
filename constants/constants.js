07264bb102a304e9fd08eb4c0b6697ccd6f1f5e9
Bruno Colombini <bruno.colombini@me.com>
2018-09-18 20:52:46 -0300

    Create Constants

---
+++ constants/constants.js
@@ -0,0 +1,53 @@
+const VERSION = "0.0.0.11";
+const LOCATION = system.io.path.getdirectoryname(system.reflection.assembly.getentryassembly().location) + "\\";
+const BITMEXKEY = "";
+const BITMEXSECRET = "";
+const BITMEXKEYWEB = "";
+const BITMEXSECRETWEB = "";
+const TIMEGRAPH = "";
+const STATUSSHORT = "";
+const STATUSLONG = "";
+const PAIR = "";
+const QTDYCONTACTS = 0;
+const INTERVAL = 0;
+const INTERVALORDER = 0;
+const INTERVALCAPTURE = 0;
+const INTERVALCANCELORDER = 30;
+const POSITIONCONTRACTS = 0;
+const PROFIT = 0;
+const LIMITEORDER = 0;
+const FEE = 0;
+const STOPLOSS = 10;
+const STOPGAIN = 15;
+const BITMEXDOMAIN = "";
+const ROEAUTOMATIC = true;
+const ROE = 0;
+
+// public static TendencyMarket tendencyMarket = TendencyMarket.NORMAL;
+// public static BitMEX.BitMEXApi bitMEXApi = null;
+
+modules.exports = {
+    VERSION,
+    LOCATION,
+    BITMEXKEY,
+    BITMEXSECRET,
+    BITMEXKEYWEB,
+    BITMEXSECRETWEB,
+    TIMEGRAPH,
+    STATUSSHORT,
+    STATUSLONG,
+    PAIR,
+    QTDYCONTACTS,
+    INTERVAL,
+    INTERVALORDER,
+    INTERVALCAPTURE,
+    INTERVALCANCELORDER,
+    POSITIONCONTRACTS,
+    PROFIT,
+    LIMITEORDER,
+    FEE,
+    STOPLOSS,
+    STOPGAIN,
+    BITMEXDOMAIN,
+    ROEAUTOMATIC,
+    ROE}
